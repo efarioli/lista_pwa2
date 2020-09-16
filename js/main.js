4251b9b6e0a6cc36e7a7f65d6ceb1b59ab4cc8ea
efarioli <efarioli@gmail.com>
2020-09-16 22:48:24 +0100

    replacing jquery for vanillaJS

--- js/main.js
+++ js/main.js
@@ -1,20 +1,53 @@
 let listaProductos = []
 const endpoint = 'https://5f61ec3889dbd70016e190fa.mockapi.io/lista'
+const s = selector => document.querySelector(selector)
+const a = selector => [...document.querySelectorAll(selector)]
 
-function borrarProducto(id) {
-  console.log(id)
-  deleteProductoWeb(id, () => {
-    renderLista()
-  })
+class ajax2 {
+  constructor(url, { queryString = '', method = 'get', data, responseType = 'json' } = {}) {
+    this.url = url
+    this.method = method
+    this.data = data
+    this.responseType = responseType
+    this.queryString = queryString
+  }
+  static objectToStrParam(obj) {
+    let str = ''
+    for (var key in obj) {
+      if (str != '') {
+        str += '&'
+      }
+      str += key + '=' + encodeURIComponent(obj[key])
+    }
+    return str
+  }
+  response() {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest()
+      xhr.responseType = this.responseType
+      xhr.open(this.method, `${this.url}${this.queryString}`)
+      xhr.addEventListener('load', () => {
+        resolve(xhr.response)
+      })
+      xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+      xhr.send(ajax2.objectToStrParam(this.data))
+    })
+  }
 }
+let doAjax = (xhr, callback) => {
+  xhr
+    .response()
+    .then(callback)
+    .catch(err => console.log(err))
+}
+
+const borrarProducto = id => deleteProductoWeb(id, renderLista)
 
 function cambiarCantidad(id, e) {
   const cantidad = Number(e.value)
   const index = listaProductos.findIndex(prod => prod.id == id)
-
   listaProductos[index].cantidad = cantidad
   const nuevoProducto = listaProductos[index]
-
   updateProductoWeb(id, nuevoProducto, () => {
     console.log('Modificado correctamente')
   })
@@ -33,75 +66,66 @@ function cambiarPrecio(id, e) {
 }
 
 function configurarListeners() {
-  $('#btn_entrada_producto').click(() => {
-    const inputProducto = $('#ingreso_producto')
+  s('#btn_entrada_producto').addEventListener('click', () => {
+    const inputProducto = s('#ingreso_producto')
 
-    let prod = inputProducto.val()
+    let prod = inputProducto.value
     if (prod !== '') {
-      const producto = {
-        nombre: prod,
-        cantidad: 0,
-        precio: 0
-      }
-
-      inputProducto.val('')
+      const producto = { nombre: prod, cantidad: 0, precio: 0 }
+      inputProducto.value = ''
       inputProducto.focus()
-
-      postProductoWeb(producto, prod => {
-        renderLista()
-      })
+      postProductoWeb(producto, renderLista)
     }
   })
-  $('#btn_borrar_todos_productos').click(() => {
-    listaProductos.forEach(element => {
-      deleteProductoWeb(element.id, renderLista)
-    })
+  s('#btn_borrar_todos_productos').addEventListener('click', () => {
     listaProductos = []
+    renderLista()
   })
 }
 
 function renderLista() {
-  $.get('templates/lista-productos.hbs', source => {
-    const template = Handlebars.compile(source)
-    getProductosWeb(productosResponse => {
-      listaProductos = productosResponse
-      $('#lista').html(template({ listaProductos }))
-      console.log(listaProductos)
-      componentHandler.upgradeElements($('#lista'))
-    })
-  }).fail(err => {
-    console.error('Error al intentar traer el template')
+  const xhr = new ajax2('templates/lista-productos.hbs', { responseType: 'text' })
+  console.log(xhr)
+
+  xhr.response().then(source => {
+    console.log(source)
+    var template = Handlebars.compile(source)
+    console.log(template)
+    getProductosWeb2()
+      .response()
+      .then(respuesta => {
+        listaProductos = respuesta
+        s('#lista').innerHTML = template({ listaProductos })
+        componentHandler.upgradeElements(s('#lista'))
+      })
+      .catch(err => {
+        console.log(err)
+      })
   })
 }
 
 function getProductosWeb(callback) {
-  $.get(endpoint, callback).fail(err => {
-    console.log(err)
-  })
+  const xhr = new ajax2(endpoint)
+  doAjax(xhr, callback)
+}
+function getProductosWeb2() {
+  return new ajax2(endpoint)
+  //   doAjax(xhr, callback)
 }
 
 function deleteProductoWeb(id, callback) {
-  $.ajax({ url: `${endpoint}/${id}`, method: 'delete' })
-    .then(callback)
-    .fail(err => {
-      console.log(err)
-    })
+  const xhr = new ajax2(endpoint, { method: 'delete', queryString: `/${id}` })
+  doAjax(xhr, callback)
 }
 
 function updateProductoWeb(id, producto, callback) {
-  $.ajax({ url: `${endpoint}/${id}`, data: producto, method: 'put' })
-    .then(callback)
-    .fail(err => {
-      console.log(err)
-    })
+  const xhr = new ajax2(endpoint, { method: 'put', data: producto, queryString: `/${id}` })
+  doAjax(xhr, callback)
 }
 
 function postProductoWeb(producto, callback) {
-  $.ajax({ url: `${endpoint}`, data: producto, method: 'post' })
-    .then(callback)
-    .fail(err => {
-      console.log(err)
-    })
+  const xhr = new ajax2(endpoint, { method: 'post', data: producto })
+  doAjax(xhr, callback)
 }
 
 function registrarServiceWorker() {
@@ -123,11 +147,6 @@ function start() {
   registrarServiceWorker()
   renderLista()
   configurarListeners()
-  const btnBorrarTodosProductos = document.querySelector('#btn_borrar_todos_productos')
-
-  btnBorrarTodosProductos.addEventListener('click', () => {
-    console.log(listaProductos)
-  })
 }
 
-$(document).ready(start)
+window.addEventListener('load', start)
